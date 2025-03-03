aa48e3a3bdc2348b1015572465647e4108a749f8
6seconds <127827678+6seconds@users.noreply.github.com>
2025-03-03 20:25:11 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,27 +1,10 @@
-@ECHO OFF
+@echo off
+netsh interface ip set dns name="Ethernet" source=static addr=1.1.1.3
+netsh interface ip add dns name="Ethernet" addr=1.0.0.3 index=2
+netsh interface ipv6 set dns name="Ethernet" source=static addr=2606:4700:4700::1113
+netsh interface ipv6 add dns name="Ethernet" addr=2606:4700:4700::1003 index=2
 
-SET "Username=mrpond"
-SET "Repository=BlockTheSpot"
-
-SET "Destination=%APPDATA%\SPOTIFY"
-SET "URLFile=%~dp0latest_url.txt"
-SET "ZipFile=%~dp0downloaded.zip"
-SET "ExtractPath=%~dp0extracted"
-
-powershell -Command "$url = 'https://api.github.com/repos/%Username%/%Repository%/releases/latest'; $json = Invoke-RestMethod $url; $latestURL = $json.assets | Where-Object { $_.name -like '*.zip' } | Select-Object -ExpandProperty browser_download_url; Set-Content -Path '%URLFile%' -Value $latestURL"
-
-SET /P "LatestURL=" < "%URLFile%"
-
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%LatestURL%', '%ZipFile%')"
-
-IF NOT EXIST "%ExtractPath%" (
-MKDIR "%ExtractPath%"
-)
-powershell -Command "Expand-Archive -Path '%ZipFile%' -DestinationPath '%ExtractPath%' -Force"
-
-DEL "%ZipFile%"
-
-ROBOCOPY "%ExtractPath%" "%Destination%" /E /V /TEE /MOVE
-
-DEL "%URLFile%"
-RD /S /Q "%ExtractPath%"
+netsh interface ip set dns name="Wi-Fi" source=static addr=1.1.1.3
+netsh interface ip add dns name="Wi-Fi" addr=1.0.0.3 index=2
+netsh interface ipv6 set dns name="Wi-Fi" source=static addr=2606:4700:4700::1113
+netsh interface ipv6 add dns name="Wi-Fi" addr=2606:4700:4700::1003 index=2
