3ecbd9b86470fec4c4298186d3ed093c18966108
6seconds <127827678+6seconds@users.noreply.github.com>
2025-03-03 22:01:22 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,6 +9,6 @@ netsh interface ip add dns name="Wi-Fi" addr=1.0.0.3 index=2
 netsh interface ipv6 set dns name="Wi-Fi" source=static addr=2606:4700:4700::1113
 netsh interface ipv6 add dns name="Wi-Fi" addr=2606:4700:4700::1003 index=2
 
-netsh advfirewall firewall delete rule name="Force SafeSearch"
+netsh advfirewall firewall add rule name="Force SafeSearch" dir=out action=block remoteip=216.239.38.120,216.239.38.119 enable=yes
 ipconfig /flushdns
 exit
