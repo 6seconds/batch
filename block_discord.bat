47959b43eac317088a80c95515b36d2e7cfb6fb9
6seconds <127827678+6seconds@users.noreply.github.com>
2025-03-03 23:37:36 +0530

    Update block_discord.bat

--- block_discord.bat
+++ block_discord.bat
@@ -1,2 +1,12 @@
-netsh advfirewall firewall add rule name="Block Discord App" dir=out action=block program="C:\Users\%USERNAME%\AppData\Local\Discord\Update.exe" enable=yes
-netsh advfirewall firewall add rule name="Block Discord App" dir=out action=block program="C:\Users\%USERNAME%\AppData\Local\Discord\app-*" enable=yes
+@echo off
+netsh advfirewall firewall add rule name="Block Discord Network" dir=out action=block remoteip=162.159.128.0/24,162.159.129.0/24,162.159.130.0/24,162.159.133.0/24,162.159.134.0/24,162.159.135.0/24,104.16.0.0/12,104.17.0.0/12,104.18.0.0/12,104.19.0.0/12,104.20.0.0/12,104.21.0.0/12 enable=yes
+(
+    echo 127.0.0.1 discord.com
+    echo 127.0.0.1 www.discord.com
+    echo 127.0.0.1 cdn.discordapp.com
+    echo 127.0.0.1 discordapp.com
+    echo 127.0.0.1 discord.gg
+) >> "%SystemRoot%\System32\drivers\etc\hosts"
+
+ipconfig /flushdns
+exit
