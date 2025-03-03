abdf32241f34b00a85788a82caa9794f30ad09f2
6seconds <127827678+6seconds@users.noreply.github.com>
2025-03-03 21:05:18 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,12 +9,7 @@ netsh interface ip add dns name="Wi-Fi" addr=1.0.0.3 index=2
 netsh interface ipv6 set dns name="Wi-Fi" source=static addr=2606:4700:4700::1113
 netsh interface ipv6 add dns name="Wi-Fi" addr=2606:4700:4700::1003 index=2
 
-netsh advfirewall firewall delete rule name="Block Non-Cloudflare DNS"
-
-netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out action=block protocol=UDP remoteport=53
-netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out action=block protocol=TCP remoteport=53
+netsh advfirewall firewall delete rule name="Force SafeSearch"
 
 ipconfig /flushdns
-echo DNS settings updated successfully!
-pause
 exit
