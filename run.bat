91612f3c57c3ff788d5f4dd1c5276ebf41bff40d
6seconds <127827678+6seconds@users.noreply.github.com>
2025-03-03 20:51:16 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,27 +1,15 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /f "tokens=1,2 delims=:" %%A in ('netsh interface show interface ^| findstr /I "connected"') do (
-    set "interfaceName=%%B"
-    set "interfaceName=!interfaceName:~1!"
-    
-    if /I "!interfaceName!"=="Ethernet" (
-        goto setdns
-    ) 
-    if /I "!interfaceName!"=="Wi-Fi" (
-        goto setdns
-    )
-)
+netsh interface ip set dns name="Ethernet" source=static addr=1.1.1.3
+netsh interface ip add dns name="Ethernet" addr=1.0.0.3 index=2
+netsh interface ipv6 set dns name="Ethernet" source=static addr=2606:4700:4700::1113
+netsh interface ipv6 add dns name="Ethernet" addr=2606:4700:4700::1003 index=2
 
-echo No active Ethernet or Wi-Fi connection found.
-pause
-exit
-
-:setdns
-netsh interface ip set dns name="!interfaceName!" source=static addr=1.1.1.3
-netsh interface ip add dns name="!interfaceName!" addr=1.0.0.3 index=2
-netsh interface ipv6 set dns name="!interfaceName!" source=static addr=2606:4700:4700::1113
-netsh interface ipv6 add dns name="!interfaceName!" addr=2606:4700:4700::1003 index=2
+netsh interface ip set dns name="Wi-Fi" source=static addr=1.1.1.3
+netsh interface ip add dns name="Wi-Fi" addr=1.0.0.3 index=2
+netsh interface ipv6 set dns name="Wi-Fi" source=static addr=2606:4700:4700::1113
+netsh interface ipv6 add dns name="Wi-Fi" addr=2606:4700:4700::1003 index=2
 
 netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out action=block protocol=UDP remoteport=53
 netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out action=block protocol=TCP remoteport=53
