490811fbc25dfa8c589c184d7b7c7d5b334175e1
6seconds <127827678+6seconds@users.noreply.github.com>
2025-03-03 21:00:26 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,7 +9,11 @@ netsh interface ip add dns name="Wi-Fi" addr=1.0.0.3 index=2
 netsh interface ipv6 set dns name="Wi-Fi" source=static addr=2606:4700:4700::1113
 netsh interface ipv6 add dns name="Wi-Fi" addr=2606:4700:4700::1003 index=2
 
-netsh advfirewall firewall add rule name="Force SafeSearch" dir=out action=block remoteip=216.239.38.120,216.239.38.119 enable=yes
+netsh advfirewall firewall delete rule name="Force SafeSearch"
+netsh advfirewall firewall delete rule name="Block Non-Cloudflare DNS"
+
+netsh advfirewall firewall add rule name="Force SafeSearch" dir=out action=block remoteip=8.8.8.8,8.8.4.4 enable=yes
+
 netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out action=block protocol=UDP remoteport=53
 netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out action=block protocol=TCP remoteport=53
 
@@ -18,7 +22,7 @@ netsh advfirewall firewall add rule name="Block Non-Cloudflare DNS" dir=out acti
     echo 216.239.38.120 google.com
     echo 204.79.197.220 www.bing.com
     echo 204.79.197.220 bing.com
-) >> "%SystemRoot%\System32\drivers\etc\hosts"
+) > "%SystemRoot%\System32\drivers\etc\hosts"
 
 ipconfig /flushdns
 echo DNS settings updated successfully!
